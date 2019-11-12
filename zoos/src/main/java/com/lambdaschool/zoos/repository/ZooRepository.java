e8f052e8c3cde3799cb6231ad6ff9d893b8bbdbe
msmaitran <itsmaisn@gmail.com>
2019-11-12 00:15:44 -0800

    Assignment

---
+++ zoos/src/main/java/com/lambdaschool/zoos/repository/ZooRepository.java
@@ -0,0 +1,19 @@
+package com.lambdaschool.zoos.repository;
+
+import com.lambdaschool.zoos.models.Zoo;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ZooRepository extends CrudRepository<Zoo, Long> {
+
+    Zoo findByZooname(String zooname);
+
+    List<Zoo> findByZoonameContaining(String zoonmae);
+
+    @Query(value = "SELECT z.zooname as zoonamerpt, count(t.phoneid) as countphone FROM zoos z JOIN telephones t ON z.zooid = t.zooid GROUP BY z.zooname",
+            nativeQuery = true)
+    List<ZooCountTelephones> getCountZooanimals();
+
+}
